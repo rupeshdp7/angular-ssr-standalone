406e184046553272670c8fd1305992d716ea6e02
Rupesh Pawar <rupesh.pawaar@gmail.com>
2024-11-07 01:10:37 +0530

    Dependency Injection router outlet added

--- src/app/dependency-injection/dependency-injection.component.ts
+++ src/app/dependency-injection/dependency-injection.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-dependency-injection',
   standalone: true,
-  imports: [],
+  imports: [RouterOutlet],
   templateUrl: './dependency-injection.component.html',
   styleUrl: './dependency-injection.component.scss'
 })
